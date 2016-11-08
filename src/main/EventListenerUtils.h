9d58ca68455bf0c98c2d1bb42f637ede0eb984c2
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-11-08 13:47:00 -0500

    Show inline test failures

--- src/main/EventListenerUtils.h
+++ src/main/EventListenerUtils.h
@@ -10,7 +10,7 @@ namespace DefaultEventListener {
     const bool SHOW_TEST_CASES = false;
     const bool SHOW_TEST_NAMES = false;
     const bool SHOW_SUCCESSES = false;
-    const bool SHOW_INLINE_FAILURES = false;
+    const bool SHOW_INLINE_FAILURES = true;
 }
 
 namespace EventListenerUtils {
